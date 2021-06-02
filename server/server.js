5b431f4098f8e72d0b9f82cc06abcb961daf5235
Summer-Shen <1563407190@qq.com>
2021-06-03 00:29:01 +0800

    implement interfaces & types in schema

--- server/server.js
+++ server/server.js
@@ -1,3 +1,38 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
-const schema = gql();
+const schema = gql(`
+  type Query {
+    
+  }
+
+  type Mutation {
+
+  }
+
+  type Intersection {
+    id: ID!
+    name: String!
+    roads: [Road!]!
+    traffic: Int!
+  }
+  
+  interface Road {
+    id: ID!
+    name: String!
+    isOneway: Boolean!
+  }
+
+  type Highway implements Road {
+    id: ID!
+    name: String!
+    isOneway: Boolean!
+    code: String!
+  }
+
+  type UrbanRoad implements Road {
+    id: ID!
+    name: String!
+    isOneway: Boolean!
+    locations: [String]
+  }
+`);
