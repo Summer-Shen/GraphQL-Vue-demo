0b1f3b785b4eb5496b928b1f4d448724723b0d01
Summer-Shen <1563407190@qq.com>
2021-06-03 14:55:35 +0800

    add apollo client

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,23 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
 import { router } from "./router";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: "http://localhost:4000",
+  }),
+});
 
 new Vue({
   el: "#app",
   router,
+  apolloProvider,
   render: (h) => h(App),
 });
