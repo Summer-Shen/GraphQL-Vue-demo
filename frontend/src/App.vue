1fd510249dda30cec07a20533d3906085bf01f72
Summer-Shen <1563407190@qq.com>
2021-06-03 12:48:22 +0800

    implement a basic vue view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,68 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-link to="/"></router-link>
+  <router-view v-if="isRouterAlive" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  provide() {
+    return {
+      reload: this.reload,
+    };
+  },
+  data() {
+    return {
+      isRouterAlive: true,
+    };
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(() => {
+        this.isRouterAlive = true;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.padded-small {
+  padding: 8px;
+}
+.inline-form {
+  margin-bottom: 0 !important;
+}
+.box-card {
+  width: 400px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+}
+.text-button {
+  padding-top: 0 !important;
+  padding-bottom: 0 !important;
+  min-height: auto !important;
+}
+.card-text {
+  font-size: 14px;
+  margin-bottom: 18px;
+}
+.card-text:last-child {
+  font-size: 14px;
+  margin-bottom: 0;
+}
+.el-table__fixed-right {
+  height: 100% !important;
+}
+.chart {
+  height: 400px;
+  padding: 10px;
+  margin: 10px;
 }
 </style>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container class="home">
+    <el-header></el-header>
+    <el-main></el-main>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
